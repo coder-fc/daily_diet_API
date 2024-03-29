b1f4c6bbb65ecb75fcd3fc6b75551c6647dfadf3
DevFC-M <felipech3344@gmail.com>
2024-03-29 18:08:01 -0300

    Creating the DB & snack model

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 from flask import Flask, request, jsonify
 from models.snack import Snack
+from database import db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "my_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/diet-crud'
+
+db.init_app(app)
--- models/snack.py
+++ models/snack.py
@@ -1,6 +1,12 @@
 from database import db
+from datetime import datetime
 
 
 class Snack(db.Model):
-    # id (int), snackname (text), description, date, diet
+    # id (int), name (text), description, date, diet
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    diet = db.Column(db.Boolean, default=True)
