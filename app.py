88702d2f61fb50ec1439557269c6444ec4a9bdb3
DevFC-M <felipech3344@gmail.com>
2024-03-29 19:50:27 -0300

    Creating the login method and updating the function to add new meals

--- app.py
+++ app.py
@@ -1,30 +1,90 @@
+import bcrypt
 from flask import Flask, request, jsonify
 from models.snack import Snack
+from models.user import User
 from database import db
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/diet-crud'
 
+login_manager = LoginManager()
 db.init_app(app)
+
+login_manager.init_app(app)
+
+# view login
+login_manager.login_view = 'login'
+# Session <- conexão ativa
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        # Login
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Autenticação realizada com sucesso"})
+
+    return jsonify({"message": "Credênciais inválidas"}), 400
+
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return ({"message": "Logout realizado com sucesso!"})
+
+
+@app.route('/create_user', methods=["POST"])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuário cadastrado com sucesso"})
+
+    return jsonify({"message": "Dados inválidos"}), 400
 
 
 @app.route('/create_snack', methods=["POST"])
 def create_snack():
-    data = request.json
-    username = data.get("username")
-    name = data.get("name")
-    description = data.get("description")
-    date = data.get("date")
+    if current_user.is_authenticated:
+        data = request.json
+        user_id = current_user.id
+        name = data.get("name")
+        description = data.get("description")
+        date = data.get("date")
+        diet = data.get("diet")
 
-    if username and name:
-        snack = Snack(username=username, name=name,
-                      description=description, date=date, diet=True)
-        db.session.add(snack)
-        db.session.commit()
-        return jsonify({"message": "Refeição cadastrada com sucesso!!!"})
+        if name and date:
+            snack = Snack(name=name,
+                          description=description, date=date, diet=diet, user_id=user_id)
+            db.session.add(snack)
+            db.session.commit()
+            return jsonify({"message": "Refeição cadastrada com sucesso!!!"})
 
-    return jsonify({"message": "Erro ao cadastrar refeição"}), 400
+        return jsonify({"message": "Erro ao cadastrar refeição"}), 400
+    else:
+        return jsonify({"message": "Usuário não autenticado"}), 401
 
 
 if __name__ == '__main__':
--- models/snack.py
+++ models/snack.py
@@ -1,12 +1,11 @@
 from database import db
-from datetime import datetime
 
 
 class Snack(db.Model):
     # id (int), name (text), description, date, diet
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
     name = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(300), nullable=False)
+    description = db.Column(db.String(300))
     date = db.Column(db.String(10), nullable=False)
     diet = db.Column(db.Boolean, default=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
