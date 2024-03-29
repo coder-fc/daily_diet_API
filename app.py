127924b04c4df57d49dbf1ce42e16f3f30044a9b
DevFC-M <felipech3344@gmail.com>
2024-03-29 18:45:12 -0300

    Updade model snack & creating route /create-snack

--- app.py
+++ app.py
@@ -7,3 +7,25 @@ app.config['SECRET_KEY'] = "my_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/diet-crud'
 
 db.init_app(app)
+
+
+@app.route('/create_snack', methods=["POST"])
+def create_snack():
+    data = request.json
+    username = data.get("username")
+    name = data.get("name")
+    description = data.get("description")
+    date = data.get("date")
+
+    if username and name:
+        snack = Snack(username=username, name=name,
+                      description=description, date=date, diet=True)
+        db.session.add(snack)
+        db.session.commit()
+        return jsonify({"message": "Refeição cadastrada com sucesso!!!"})
+
+    return jsonify({"message": "Erro ao cadastrar refeição"}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- models/snack.py
+++ models/snack.py
@@ -5,8 +5,8 @@ from datetime import datetime
 class Snack(db.Model):
     # id (int), name (text), description, date, diet
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False, unique=True)
+    username = db.Column(db.String(80), nullable=False)
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(300), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date = db.Column(db.String(10), nullable=False)
     diet = db.Column(db.Boolean, default=True)
